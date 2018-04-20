03855d8f2f90480dad8676bbc957fbd9748f9288
Mitch Kroska <kromitj@gmail.com>
2018-04-19 22:09:28 -0500

    Refactor user register2

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+
 const User = require('../../models/User')
 
-// @route 	GET /api/users/test
-// @desc   Test user route 
-// @access Public
-router.get('/test', (req, res) => res.json({blah: "blah", crazy: "Users WOrks"}) );
+const router = express.Router();
 
-// @route 	POST /api/users/register
-// @desc   Register user 
-// @access Public
+
 router.post('/register', (req, res) => { 
   User.findOne({ email: req.body.email})
   .then((user) => {
@@ -19,8 +14,8 @@ router.post('/register', (req, res) => {
   		return res.status(400).json({ error: "Email Exists", message: "Email already being used"})
   	} else {
   		populateUserParams(req.body)		  
-		  .then(params => {
-			  const newUser = new User(params)
+		  .then(userParams => {
+			  const newUser = new User(userParams)
 			  newUser.save()
 			  .then(user => res.json(user))
 			  .catch(err => console.log(err))		  	
@@ -33,16 +28,9 @@ const populateUserParams = (reqBody) => {
 	return new Promise((resolve, reject) => {
 		genBcrypt(reqBody.password)
 		.then((hash, err) => {
-		  const params = {
-		 	 name: reqBody.name,
-			 email: reqBody.email,
-			 avatar: gravatar.url({s: '200', r: 'pg', d: 'mm'}),
-			 password: hash
-		  };
-		 if (hash) {
-		 	resolve(params)
-		 }
-		 else { reject(err)}		 
+			const params = {name: reqBody.name, email: reqBody.email, avatar: gravatar.url({s: '200', r: 'pg', d: 'mm'}), password: hash };
+			if (hash) { resolve(params) }
+		 	else { reject(err)};		 
 		})
 	})
 }
@@ -51,13 +39,10 @@ const genBcrypt = (password, userParams) => {
 	return new Promise((resolve, reject)=>{
 		bcrypt.genSalt(10, (err, salt) => {
 			bcrypt.hash(password, salt, (err, hash) => {
-				if (hash) {
-					resolve(hash)
-				}
-				else { reject(err)}
+				if (hash) { resolve(hash) }
+				else { reject(err) }
 			})
 		})		
 	})
-
 }
 module.exports = router;
